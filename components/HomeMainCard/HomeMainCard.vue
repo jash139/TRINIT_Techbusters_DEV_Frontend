4700f5edbdaac577af0c4b2e5f5710e63cb162ac
Ayush Acharya <ayushjash1323@gmail.com>
2023-02-11 22:06:38 +0530

    Add NGODisplayCard, SearchResults

--- components/HomeMainCard/HomeMainCard.vue
+++ components/HomeMainCard/HomeMainCard.vue
@@ -2,19 +2,22 @@
   <div class="home-main-card">
     <div class="search-bar">Search bar</div>
     <TopNGOs />
+    <SearchResults />
   </div>
 </template>
 
 <script>
+import SearchResults from "./SearchResults.vue";
 import TopNGOs from "./TopNGOs.vue";
 export default {
-  components: { TopNGOs },
+  components: { TopNGOs, SearchResults },
 };
 </script>
 
 <style scoped>
 .home-main-card {
   background-color: white;
+  border-radius: 5px;
   max-width: 750px;
   width: 100%;
 }
---
+++ components/HomeMainCard/SearchResults.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="search-results">
+    <button type="button">Search Results (2)</button>
+    <div class="content">
+      <NGODisplayCard />
+      <NGODisplayCard />
+      <NGODisplayCard />
+    </div>
+  </div>
+</template>
+
+<script>
+import NGODisplayCard from "./NGODisplayCard.vue";
+
+export default {
+  components: { NGODisplayCard },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.search-results {
+  padding: 1rem;
+}
+.search-results button {
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  background-color: white;
+  font-size: 1.25rem;
+  padding: 1rem;
+}
+.content {
+  padding: 0 1rem;
+  display: block;
+  overflow: hidden;
+}
+</style>
