e3d1c2ca95783128891013f927d31dc3498558b0
Ayush Acharya <ayushjash1323@gmail.com>
2023-02-12 01:33:43 +0530

    Update

--- components/HomeMainCard/HomeMainCard.vue
+++ components/HomeMainCard/HomeMainCard.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home-main-card">
-    <div class="search-bar">Search bar</div>
+    <div class="search-bar">
+      <input
+        class="search-box"
+        type="text"
+        placeholder="Search"
+        name="search"
+        v-model="searchValue"
+        @change="updateSearchResults"
+      />
+    </div>
     <TopNGOs />
     <SearchResults />
   </div>
@@ -11,6 +20,16 @@ import SearchResults from "./SearchResults.vue";
 import TopNGOs from "./TopNGOs.vue";
 export default {
   components: { TopNGOs, SearchResults },
+  data() {
+    return {
+      searchValue: "",
+    };
+  },
+  methods: {
+    updateSearchResults() {
+      // call api to update search results and pass as prop to <SearchResults />
+    },
+  },
 };
 </script>
 
@@ -19,6 +38,17 @@ export default {
   background-color: white;
   border-radius: 5px;
   max-width: 750px;
+  width: 100%;
+}
+.search-bar {
+  padding: 1.5rem;
+}
+.search-box {
+  border: none;
+  outline: none;
+  padding: 1rem;
+  background-color: rgb(241, 241, 241);
+  border-radius: 5rem;
   width: 100%;
 }
 </style>
