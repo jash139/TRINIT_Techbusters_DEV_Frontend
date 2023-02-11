da31b3434cacdbfac82b3702f33eb56474334467
Ayush Acharya <ayushjash1323@gmail.com>
2023-02-11 16:58:14 +0530

    Add pages

---
+++ pages/community/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>search</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
-  <Tutorial/>
+  <div>
+    <TheNavbar />
+    <h1>Hello owlrd</h1>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
